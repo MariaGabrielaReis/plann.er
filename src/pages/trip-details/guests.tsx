d141c8aa3931a2e509bcfb9969928a3eb6c18cf9
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2024-07-30 10:43:11 -0300

    feat: add guests list component

---
+++ src/pages/trip-details/guests.tsx
@@ -0,0 +1,28 @@
+import { CircleDashed, UserCog } from "lucide-react";
+
+export function Guests() {
+  return (
+    <div className="space-y-6">
+      <h2 className="font-semibold text-xl">Convidados</h2>
+
+      <div className="space-y-5">
+        <div className="flex items-center justify-between gap-4">
+          <div className="space-y-1.5">
+            <span className="block font-medium text-zinc-100">
+              Wallace Felipe
+            </span>
+            <span className="block text-sm text-zinc-400 truncate">
+              wllc@email.com
+            </span>
+          </div>
+          <CircleDashed className="size-5 text-zinc-400 shrink-0" />
+        </div>
+      </div>
+
+      <button className="bg-zinc-800 w-full text-zinc-200 rounded px-5 h-10 font-medium flex items-center justify-center gap-2 hover:bg-zinc-700">
+        <UserCog className="size-5" />
+        Gerenciar convidados
+      </button>
+    </div>
+  );
+}
