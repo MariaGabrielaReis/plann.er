3677462c5a8f0b70d2368bb97a19bc4558b13d96
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2024-08-01 14:20:04 -0300

    feat: list activities

--- src/pages/trip-details/activities.tsx
+++ src/pages/trip-details/activities.tsx
@@ -1,32 +1,60 @@
-import { CircleCheck } from "lucide-react";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { CheckCircle } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { api } from "../../lib/axios";
+
+type Activity = {
+  date: string;
+  activities: { id: string; title: string; occurs_at: string }[];
+};
 
 export function Activities() {
+  const { tripId } = useParams();
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    api
+      .get(`/trips/${tripId}/activities`)
+      .then(response => setActivities(response.data.activities));
+  }, [tripId]);
+
   return (
     <div className="space-y-8">
-      <div className="space-y-2.5">
-        <div className="flex gap-2 items-baseline">
-          <span className="text-xl text-zinc-300 font-semibold">Dia 7</span>
-          <span className="text-sm text-zinc-500">Sexta</span>
-        </div>
-        <p className="text-zinc-500">
-          Nenhuma atividade cadastrada nessa data.
-        </p>
-      </div>
-
-      <div className="space-y-2.5">
-        <div className="flex gap-2 items-baseline">
-          <span className="text-xl text-zinc-300 font-semibold">Dia 8</span>
-          <span className="text-sm text-zinc-500">Sábado</span>
-        </div>
-
-        <div className="space-y-2.5">
-          <div className="px-4 py-2.5 bg-zinc-900 rounded-lg shadow-shape flex items-center gap-3">
-            <CircleCheck className="size-5 text-teal-300" />
-            <span className="text-zinc-100">Academia em casal</span>
-            <span className="text-zinc-400 text-sm ml-auto">08:00</span>
+      {activities.map(activityDay => (
+        <div key={activityDay.date} className="space-y-2.5">
+          <div className="flex gap-2 items-baseline">
+            <span className="text-xl text-zinc-300 font-semibold">
+              Dia {format(activityDay.date, "d")}
+            </span>
+            <span className="text-sm text-zinc-500">
+              {format(activityDay.date, "EEEE", { locale: ptBR })}
+            </span>
           </div>
+
+          {activityDay.activities.length > 0 ? (
+            <>
+              {activityDay.activities.map(activity => (
+                <div key={activity.id} className="space-y-2.5">
+                  <div className="px-4 py-2.5 bg-zinc-900 rounded-lg shadow-shape flex items-center gap-3">
+                    <CheckCircle className="size-5 text-teal-300" />
+                    <span className="text-zinc-100">{activity.title}</span>
+                    <span className="text-zinc-400 text-sm ml-auto">
+                      {format(activity.occurs_at, "HH:mm")}
+                    </span>
+                  </div>
+                </div>
+              ))}
+            </>
+          ) : (
+            <p className="text-zinc-500">
+              Nenhuma atividade cadastrada nessa data.
+            </p>
+          )}
         </div>
-      </div>
+      ))}
     </div>
   );
 }
